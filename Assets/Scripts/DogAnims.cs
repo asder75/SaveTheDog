ff4e31d44b2237bfd9db03cb0b99dfdb6a4448c8
Stepan <asder755@mail.ru>
2022-09-20 23:07:44 +0700

    Стартовый экран
    Сделан стартовый экран
    //StartScreen

---
+++ Assets/Scripts/DogAnims.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAnims : MonoBehaviour
+{
+    public Animation dogstay;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       // if (Game.aliveDog == true)
+       // {
+            dogstay.Play("dogstay");
+       // }
+    }
+}
